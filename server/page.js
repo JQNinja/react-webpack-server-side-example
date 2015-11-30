3494b8795817f56f16c03350a7cf75c3a6dfca2a
Steven Miller <smiller@healthwise.org>
2015-11-30 11:42:59 -0700

    Boiled down to just server-side rendering

--- server/page.js
+++ server/page.js
@@ -1,17 +1,15 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server")
-var EmailHeading = require("../app/email_heading.jsx");
-
+var EmailHeading = require("../app/email_heading");
 
 module.exports = function(req) {
-
 	return ReactDOMServer.renderToStaticMarkup(
 		<html>
 			<head>
 				<title>Boo ya!</title>
 			</head>
 			<body>
-				<EmailHeading text="Boo ya!"/>
+				<EmailHeading text="Boo YO!"/>
 			</body>
 		</html>
 	);
